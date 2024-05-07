6ff525a4d28911d16ee5002da77bf87bf0cf1ac6
dosxana@gmail.com <dosxana@gmail.com>
2024-05-07 15:44:51 +0500

    Implemented caching of genres, genres_id routes

--- app/api/v1/genres.py
+++ app/api/v1/genres.py
@@ -4,37 +4,29 @@ from app.db.elastic import get_elastic, AsyncElasticsearch
 from app.models.genre import Genre
 from uuid import UUID
 
+from app.services.base import BaseService
+from app.services.genres import GenreService, get_genre_service
+
 router = APIRouter()
 
 
 @router.get("/{genre_id}", response_model=Genre)
-async def get_genre_by_id(genre_id: UUID = Path(..., description="genre's ID"),
-                          es: AsyncElasticsearch = Depends(get_elastic)) -> Genre:
-    """
-    Получение жанра по id.
-
-    - **genre_id**: id жанра
-    """
-    response = await es.get(index="genres", id=genre_id)
-    return Genre(**response['_source'])
+async def get_genre_by_id(
+        genre_id: UUID = Path(..., description="Genre's ID"),
+        service: GenreService = Depends(get_genre_service)) -> Genre:
+    return await service.get_by_id(genre_id)
 
 
 @router.get("/", response_model=list[Genre])
-async def genre(es: AsyncElasticsearch = Depends(get_elastic),
-                page_size: int = Query(10, ge=1, description='Pagination page size'),
-                page_number: int = Query(1, ge=1, description='Pagination page number'),
-
-                ) -> list[Genre]:
+async def genre(
+        page_size: int = Query(10, ge=1, description='Pagination page size'),
+        page_number: int = Query(1, ge=1, description='Pagination page number'),
+        service: GenreService = Depends(get_genre_service)
+) -> list[Genre]:
     """
     Получение списка жанров с пагинацией.
 
     - **page_size**: размер страницы
     - **page_number**: номер страницы
     """
-    offset = (page_number - 1) * page_size
-
-    response = await es.search(
-        index="genres",
-        body={"from": offset, "size": page_size},
-    )
-    return [Genre(**item['_source']) for item in response['hits']['hits']]
+    return await service.list_genres(page_size, page_number)
