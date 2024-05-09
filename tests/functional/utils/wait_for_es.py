fc8e1b66e651705596c931be9119d460fcf90ef8
dosxana <dosxana@gmail.com>
2024-05-09 18:50:48 +0500

    Implemented the launch of tests in docker

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -5,7 +5,7 @@ from elasticsearch import Elasticsearch
 
 es_host = os.getenv('ELASTIC_HOST', 'localhost')
 es_port = os.getenv('ELASTIC_PORT', '9200')
-es_url = f"http://{es_host}:{es_port}"
+es_url = f'http://{es_host}:{es_port}'
 
 if __name__ == '__main__':
     es_client = Elasticsearch(hosts=[es_url], verify_certs=False)
