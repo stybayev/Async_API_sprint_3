8a3b483ea1bce398c8ef13098a4b48166246b18c
dosxana <dosxana@gmail.com>
2024-05-09 17:18:30 +0500

    Implemented the launch of tests in docker

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -11,140 +11,22 @@ else:
 
 
 class TestSettings(BaseSettings):
-    es_host: str = Field('http://elasticsearch-test:9200', env='ELASTIC_HOST')
-    es_index: str = Field(default='movies')
-    redis_host: str = Field(default=os.getenv('REDIS_HOST', 'localhost'))
-    service_url: str = Field(default='http://fastapi:8080', env='SERVICE_URL')
-    # es_mapping = {
-    #     "movies": {
-    #         "mappings": {
-    #             "dynamic": "strict",
-    #             "properties": {
-    #                 "actors": {
-    #                     "type": "nested",
-    #                     "dynamic": "strict",
-    #                     "properties": {
-    #                         "id": {
-    #                             "type": "keyword"
-    #                         },
-    #                         "name": {
-    #                             "type": "text",
-    #                             "analyzer": "ru_en"
-    #                         }
-    #                     }
-    #                 },
-    #                 "actors_names": {
-    #                     "type": "text",
-    #                     "fields": {
-    #                         "raw": {
-    #                             "type": "keyword"
-    #                         }
-    #                     },
-    #                     "analyzer": "ru_en"
-    #                 },
-    #                 "description": {
-    #                     "type": "text",
-    #                     "analyzer": "ru_en"
-    #                 },
-    #                 "director": {
-    #                     "dynamic": "strict",
-    #                     "properties": {
-    #                         "id": {
-    #                             "type": "keyword"
-    #                         },
-    #                         "name": {
-    #                             "type": "text",
-    #                             "fields": {
-    #                                 "raw": {
-    #                                     "type": "keyword"
-    #                                 }
-    #                             },
-    #                             "analyzer": "ru_en"
-    #                         }
-    #                     }
-    #                 },
-    #                 "genre": {
-    #                     "type": "keyword"
-    #                 },
-    #                 "id": {
-    #                     "type": "keyword"
-    #                 },
-    #                 "imdb_rating": {
-    #                     "type": "float"
-    #                 },
-    #                 "title": {
-    #                     "type": "text",
-    #                     "fields": {
-    #                         "raw": {
-    #                             "type": "keyword"
-    #                         }
-    #                     },
-    #                     "analyzer": "ru_en"
-    #                 },
-    #                 "writers": {
-    #                     "type": "nested",
-    #                     "dynamic": "strict",
-    #                     "properties": {
-    #                         "id": {
-    #                             "type": "keyword"
-    #                         },
-    #                         "name": {
-    #                             "type": "text",
-    #                             "analyzer": "ru_en"
-    #                         }
-    #                     }
-    #                 },
-    #                 "writers_names": {
-    #                     "type": "text",
-    #                     "fields": {
-    #                         "raw": {
-    #                             "type": "keyword"
-    #                         }
-    #                     },
-    #                     "analyzer": "ru_en"
-    #                 }
-    #             }
-    #         }
-    #     },
-    #     "persons": {
-    #         "mappings": {
-    #             "dynamic": "strict",
-    #             "properties": {
-    #                 "full_name": {
-    #                     "type": "text",
-    #                     "fields": {
-    #                         "raw": {
-    #                             "type": "keyword"
-    #                         }
-    #                     }
-    #                 },
-    #                 "id": {
-    #                     "type": "keyword"
-    #                 }
-    #             }
-    #         }
-    #     },
-    #     "genres": {
-    #         "mappings": {
-    #             "dynamic": "strict",
-    #             "properties": {
-    #                 "description": {
-    #                     "type": "text"
-    #                 },
-    #                 "id": {
-    #                     "type": "keyword"
-    #                 },
-    #                 "name": {
-    #                     "type": "text",
-    #                     "fields": {
-    #                         "raw": {
-    #                             "type": "keyword"
-    #                         }
-    #                     }
-    #                 }
-    #             }
-    #         }
-    #     }
-    # }
+    es_host: str | None = None
+    es_index: str | None = None
+    redis_host: str | None = None
+
+    if os.path.exists('/.dockerenv'):
+        # Docker
+        es_host: str = Field('http://elasticsearch:9200')
+        es_index: str = Field(default='movies')
+        redis_host: str = Field('redis')
+        service_url: str = Field(default='http://app:8000')
+    else:
+        # Local
+        es_host: str = Field('http://127.0.0.1:9200')
+        es_index: str = Field(default='movies')
+        redis_host: str = Field('localhost')
+        service_url: str = Field('http://127.0.0.1:8000')
+
 
 test_settings = TestSettings()
