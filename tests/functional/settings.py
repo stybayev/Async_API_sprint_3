b37c94a2baad29e0c4baf97a7562e023048a32bb
dosxana <dosxana@gmail.com>
2024-05-09 18:22:07 +0500

    Implemented the launch of tests in docker

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -1,32 +1,14 @@
 import os
-
 from pydantic import Field
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
-if os.path.exists('/.dockerenv'):
-    load_dotenv()
-else:
-    load_dotenv(dotenv_path='.env.local')
-
+load_dotenv(dotenv_path='.env.test')
 
 class TestSettings(BaseSettings):
-    es_host: str | None = None
-    es_index: str | None = None
-    redis_host: str | None = None
-
-    if os.path.exists('/.dockerenv'):
-        # Docker
-        es_host: str = Field('http://elasticsearch:9200')
-        es_index: str = Field(default='movies')
-        redis_host: str = Field('redis')
-        service_url: str = Field(default='http://app:8000')
-    else:
-        # Local
-        es_host: str = Field('http://127.0.0.1:9200')
-        es_index: str = Field(default='movies')
-        redis_host: str = Field('localhost')
-        service_url: str = Field('http://127.0.0.1:8000')
-
+    es_host: str = Field(default=os.getenv('TEST_ELASTIC_HOST', 'http://127.0.0.1:9200'))
+    es_index: str = Field(default='movies')
+    redis_host: str = Field(default=os.getenv('TEST_REDIS_HOST', 'localhost'))
+    service_url: str = Field(default=os.getenv('TEST_SERVICE_URL', 'http://127.0.0.1:8000'))
 
 test_settings = TestSettings()
