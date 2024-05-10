1b8c6a50add1cc304b5980ae4223d0cfdf8ef29f
dosxana <dosxana@gmail.com>
2024-05-10 12:01:14 +0500

    Implemented the launch of tests in docker

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path='.env.test')
 
 class TestSettings(BaseSettings):
-    es_host: str = Field(default=os.getenv('TEST_ELASTIC_HOST', 'http://127.0.0.1:9200'))
+    es_host: str = Field(default=os.getenv('TEST_ELASTIC_HOST', 'http://127.0.0.1:9201'))
     es_index: str = Field(default='movies')
     redis_host: str = Field(default=os.getenv('TEST_REDIS_HOST', 'localhost'))
-    service_url: str = Field(default=os.getenv('TEST_SERVICE_URL', 'http://127.0.0.1:8000'))
+    service_url: str = Field(default=os.getenv('TEST_SERVICE_URL', 'http://127.0.0.1:8001'))
 
 test_settings = TestSettings()
