e8080ab3b24df08afb5e72c1002ee48c24d17227
dosxana <dosxana@gmail.com>
2024-05-12 01:30:53 +0500

    eliminated the comments on the test configs

--- tests/functional/wait-for-es.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Проверяем доступность Elasticsearch на порту 9200
-while ! nc -z elasticsearch 9200; do
-  echo "Waiting for Elasticsearch..."
-  sleep 1
-done
-echo "Elasticsearch is ready!"
